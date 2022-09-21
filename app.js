c4f073f9d7699928a13aad4d6d3d995fc0c62da7
Meysam Salehi <m.salehi@tamitco.ir>
2022-09-21 14:19:25 +0430

    Session 1 & 2

---
+++ app.js
@@ -0,0 +1,9 @@
+const test = () => "salam";
+let meysam = "salam";
+console.log(test());
+
+// nasbe babel preset
+// npm i -D @babel/preset-env
+
+//convert js to babel
+//npx babel app.js --out-file script-compiled.js --presets @babel/preset-env
---
+++ script-compiled.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var test = function test() {
+  return "salam";
+};
+
+var meysam = "salam";
+console.log(test()); // nasbe babel preset
+// npm i -D @babel/preset-env
+//convert js to babel
+//npx babel app.js --out-file script-compiled.js --presets @babel/preset-env
